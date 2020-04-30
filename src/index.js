1ac0a03e6a0c0cdb47f1c0798129b5efde09951d
Ahsan2 <ahsan21916@gmail.com>
2020-04-30 22:01:05 +0500

    feat: setup bootstrap. removed unneeded packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/styles/bootstrap/custom.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './index.css';
 import App from './App';
 
