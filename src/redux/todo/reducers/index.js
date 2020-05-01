7bd7251eb2261d08c134c52ea53e395ca74b2c53
Ahsan2 <ahsan21916@gmail.com>
2020-05-01 08:23:07 +0500

    refactor: Made todo reducer composable

--- src/redux/todo/reducers/index.js
+++ src/redux/todo/reducers/index.js
@@ -6,9 +6,46 @@ import {
 
 import { VisibilityFilters } from '../actions'
 
+const { SHOW_ALL } = VisibilityFilters
+
 const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  visibilityFilter: SHOW_ALL,
   todos: []
+}
+
+const todos = (state = [], action) => {
+  const { type, payload } = action
+  switch (type) {
+    case ADD_TODO:
+      return [ 
+        ...state,
+        {
+          ...payload,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map(todo => {
+        if (todo.id === payload)
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
+const visibilityFilter = (state = SHOW_ALL, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case SET_VISIBILITY_FILTER:
+      return payload
+    default:
+      return state
+  }
 }
 
 export default (state = initialState, action) => {
@@ -17,30 +54,17 @@ export default (state = initialState, action) => {
     case ADD_TODO:
       return {
         ...state,
-        todos: [ 
-          ...state.todos,
-          {
-            ...payload,
-            completed: false
-          }
-        ]
+        todos: todos(state.todos, action)
       }
     case TOGGLE_TODO:
       return {
         ...state,
-        todos: state.todos.map(todo => {
-          if (todo.id === payload)
-            return {
-              ...todo,
-              completed: !todo.completed
-            }
-          return todo
-        })
+        todos: todos(state.todos, action)
       }
     case SET_VISIBILITY_FILTER:
       return {
         ...state,
-        visibilityFilter: payload
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action)
       }
     default:
       return state
